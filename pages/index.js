92da16ed808856cc2a9349d21cc4c1482d713195
Mark O'Connor <mark.oconnor14@gmail.com>
2022-07-22 19:36:14 +0700

    change to antd

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,56 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { Row, Col, Menu, Card } from 'antd';
 
 export default function Home() {
   return (
     <>
-    <header>
-      <div className="container flex navbar">
-        <span className="logo">Logo</span>
-        <nav>
-          <ul role="list">
-            <li>
-              <Link href={`/admin`}>
-                <a>Log In</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header style={{ backgroundColor: '#1f1f1f' }}>
+        <Row style={{flexFlow: 'nowrap', height: '64px', backgroundColor: '#1f1f1f', color: 'white', padding: '0 1rem' }}>
+          <Col span={6} style={{ display: 'flex', alignItems: 'center'}}>
+            <span style={{fontSize: '2rem'}}>Productly</span>
+          </Col>
+          <Col span={18} style={{ display: 'flex', justifyContent: 'right', alignItems: 'center'}}>
+            <Link href={`/admin`}>
+              <a>Log In</a>
+            </Link>
+          </Col>
+        </Row>
     </header>
     <main>
-      <section className="section bg-white welcome">
-        <div className="container">
+      <section style={{ backgroundColor: 'white' }}>
+        <Card 
+          bordered={false}
+          style={{ minHeight: '75vh', maxWidth: '1200px', margin: 'auto' }}
+          className='grid-center'
+        >
           <h1>Welcome, New User, this is the login page.</h1>
           <p>Please click Log In to go to your dashboard.</p>
           <p>Authentication coming soon.</p>
-        </div>
+        </Card>
       </section>
     </main>
-    <footer className="footer">
-      <div className="container">
+    <footer 
+      style={{ backgroundColor: '#1f1f1f' }}
+    >
+      <Card 
+        bordered={false}
+        style={{ minHeight: '25vh', backgroundColor: '#1f1f1f', color: 'white' }}
+      >
         <p>
-          Developer Contact:{" "}
-          <a href="mailto:markodevo14@gmail.com">markodevo14@gmail.com</a>
+          Developer Contact:{' '}
+          <a href='mailto:markodevo14@gmail.com'>markodevo14@gmail.com</a>
         </p>
         <p>
-          Github:{" "}
+          Github:{' '}
           <a 
-            href="https://github.com/markoco14"
-            target="_about"
-            rel="noopenner noreferrer"
+            href='https://github.com/markoco14'
+            target='_about'
+            rel='noopenner noreferrer'
           >
             Markoco14
           </a>
         </p>
-      </div>
+      </Card>
     </footer>
 
     </>
