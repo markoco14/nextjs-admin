1ac8d960a1d71682f00666488d441b84c57bbd19
Mark O'Connor <mark.oconnor14@gmail.com>
2022-07-25 14:55:10 +0700

    set mobile and desktop navigation to inline and fix menu key errors

--- components/adminLayout.js
+++ components/adminLayout.js
@@ -1,9 +1,19 @@
 import Link from "next/link";
-import { Row, Col, Menu, Dropdown } from 'antd';
+import { useState } from 'react';
+import { Row, Col, Menu, Button, Drawer } from 'antd';
 import { MenuOutlined } from '@ant-design/icons'
-
+import { withRouter } from "next/router";
 
 export default function Layout({ children }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const showDrawer = () => {
+        setIsVisible(true);
+    };
+
+    const onClose = () => {
+        setIsVisible(false);
+    };
 
     return (
         <>
@@ -12,62 +22,77 @@ export default function Layout({ children }) {
                     <Col span={6} style={{ display: 'flex', alignItems: 'center'}}>
                         <span style={{fontSize: '2rem'}}>Productly</span>
                     </Col>
-                    <Col span={18} style={{ display: 'flex', justifyContent: 'right', alignItems: 'center'}}>
+                    <Col span={18} style={{ display: 'flex', justifyContent: 'right', alignItems: 'center', gap: '1rem'}}>
                         <Link href={`/`}>
                             <a>Log Out</a>
                         </Link>
-                        <Menu
-                            className='show-800'
-                            mode='vertical'
-                            title='Menu'
-                            style={{ backgroundColor: '#1f1f1f' }}
-                            icon={<MenuOutlined style={{ color: 'white', width: '1rem', height: '1rem' }}/>}
+                        <Button 
+                            className="show-800"
+                            onClick={showDrawer}
+                            ghost='true'
+                            shape='circle'
                         >
-                            <Menu.SubMenu>
-                                <Menu.Item>
+                            <MenuOutlined />
+                        </Button>
+                        <Drawer
+                            width='250px'
+                            className='show-800'
+                            placement="right"
+                            visible={isVisible}
+                            onClose={onClose}
+                        >
+                            <Menu
+                                mode="inline"
+                            >
+                                <Menu.Item key='1'>
                                     <Link href={`/admin`}>
                                         <a>Home</a>
                                     </Link>
                                 </Menu.Item>
-                                <Menu.SubMenu key="SubMenu" title="Products">
-                                    <Menu.Item>
+                                <Menu.SubMenu
+                                    title="Products"
+                                    type="vertical"
+                                >
+                                    <Menu.Item key='2'>
                                         <Link href={`/admin/products`}>
                                             <a>Catalogue</a>
                                         </Link>
                                     </Menu.Item>
-                                    <Menu.Item>
+                                    <Menu.Item key='3'>
                                         <Link href={`/admin/products/manage`}>
                                             <a>Manage</a>
                                         </Link>
                                     </Menu.Item>
                                 </Menu.SubMenu>
-                                <Menu.Item>
+                                <Menu.Item key='4'>
                                     <Link href={`/admin/stats`}>
                                         <a>Stats</a>
                                     </Link>
                                 </Menu.Item>
-                            </Menu.SubMenu>
-                        </Menu>
+                            </Menu>
+                        </Drawer>
                     </Col>
                 </Row>
             </header>
             <main>
-                <Row style={{flexFlow: 'nowrap', minHeight: '100vh', backgroundColor: '#1f1f1f', color: 'white'}}>
+                <Row style={{flexFlow: 'nowrap', minHeight: '100vh'}}>
                     <Col 
-                        style={{ minWidth: '150px' }}
+                        style={{ minWidth: '200px', backgroundColor: 'white' }}
                         className="hide-800"
                     >
                         <Menu
+                            mode='inline'
                             className='hide-800'
-                            style={{ backgroundColor: '#1f1f1f' }}
+
+                            style={{ minHeight: '100%' }}
                         >
                             <Menu.Item>
                                 <Link href={`/admin`}
                                 >
-                                    <a style={{ color: 'white' }}>Home</a>
+                                    <a>Home</a>
                                 </Link>
                             </Menu.Item>
-                            <Menu.SubMenu key="SubMenu" title="Products" style={{ color: 'white' }}>
+                            <Menu.SubMenu key="SubMenu" title="Products">
                                 <Menu.Item>
                                     <Link href={`/admin/products`}>
                                         <a>Catalogue</a>
@@ -81,7 +106,7 @@ export default function Layout({ children }) {
                             </Menu.SubMenu>
                             <Menu.Item>
                                 <Link href={`/admin/stats`}>
-                                    <a style={{ color: 'white' }}>Stats</a>
+                                    <a>Stats</a>
                                 </Link>
                             </Menu.Item>
                         </Menu>
